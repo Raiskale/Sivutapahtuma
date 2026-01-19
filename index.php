e8d9dea43903881f4bc6fb82de208af4722ad5ce
Niilo Räisänen <s4rani02mkdir omarepocd omarepo>
2026-01-19 08:44:27 +0200

    Finished site

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toripäivä | Kevätmarkkina 2026</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="shortcut icon" href="public/bubel.png" type="image/x-icon">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+
+</head>
+<body>
+
+<?php include 'src/header.php'; ?>
+<?php include 'src/front.php'; ?>
+<?php include 'src/info.php'; ?>
+<?php include 'src/kolmas.php'; ?>
+
+<footer class="site-footer">
+    <div class="footer-content">
+        <p>&copy; 2026 Toripäivä | Oulu</p>
+        <p>Seuraa meitä: 
+            <a href="#" target="_blank">Facebook</a> | 
+            <a href="#" target="_blank">Instagram</a>
+        </p>
+        <p>Yhteystiedot: <a href="mailto:info@toripaiva.fi">info@toripaiva.fi</a></p>
+    </div>
+</footer>
+   <script>
+    AOS.init();
+  </script>
+
+
+<script>
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".card h3").forEach(header => {
+        header.addEventListener("click", () => {
+            header.parentElement.classList.toggle("active");
+        });
+    });
+});
+</script>
+
+
+
+
+
+
+</body>
+</html>
---
+++ src/header.php
@@ -0,0 +1,32 @@
+<?php session_start(); ?>
+
+<header class="header">
+    <div class="logo">
+        <a href="index.php">
+            <img src="Public/bubel.png" alt="">
+            Toripäivä
+        </a>
+    </div>
+
+    <input type="checkbox" id="menu-toggle">
+    <label for="menu-toggle" class="menu-icon">&#9776;</label>
+
+    <nav class="navbar">
+
+<a href="index.php#eka">Etusivu</a>
+<a href="index.php#toka">Tapahtuma</a>
+<a href="index.php#kolmas">Ohjelma</a>
+
+
+
+        <?php
+            $link = isset($_SESSION["user"]) ? "dashboard.php" : "login.php";
+            $text = isset($_SESSION["user"]) ? "Oma profiili" : "Ilmoittaudu mukaan";
+        ?>
+
+        <a id="nappi" href="<?= $link ?>">
+            <?= $text ?> <i class='bx bx-arrow-back bx-rotate-180'></i>
+        </a>
+
+    </nav>
+</header>
